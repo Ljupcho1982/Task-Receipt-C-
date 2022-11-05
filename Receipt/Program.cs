852b6cb76545ef5c7a7962162b3f3a18c77ff2b7
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-11-05 22:06:09 +0100

    Add project files.

---
+++ Receipt/Program.cs
@@ -0,0 +1,93 @@
+﻿
+
+using Newtonsoft.Json;
+using Receipt;
+
+string url = "https://interview-task-api.mca.dev/qr-scanner-codes/alpha-qr-gFpwhsQ8fkY1";
+
+HttpClient client = new HttpClient();
+
+try
+{
+    var responce = await client.GetAsync(url);
+    string JsonResponce = await responce.Content.ReadAsStringAsync();
+    //Console.WriteLine(JsonResponce);
+
+    var mypost = JsonConvert.DeserializeObject<Root[]>(JsonResponce);
+
+    //foreach (var item in mypost)
+    //{
+
+    //	//Console.WriteLine(item.price);
+
+    //	var querry=from item.
+    //}
+
+    double totalDomestic = 0;
+    double totalInternational = 0;
+    int countDomestic = 0;
+    int countInternational = 0;
+
+    var groupByOriginDomestic =
+    (from post in mypost
+     where post.domestic.Equals(true)
+     select post).OrderBy(x => x.name);
+
+    Console.WriteLine("________________________Products____________________________");
+
+
+
+    Console.WriteLine("------------------------Domestic----------------------------");
+
+    foreach (var group in groupByOriginDomestic)
+    {
+
+
+        if (group.weight == 0)
+        {
+            group.weight.ToString().Replace("0", "N/A");
+
+        }
+
+
+        Console.WriteLine($" \n {group.name}\n {group.price}$ \n {group.description} \n Weight {group.weight}g");
+
+        totalDomestic += (double)group.price;
+        countDomestic++;
+    }
+
+    var groupByOriginInernational =
+   (from post in mypost
+    where post.domestic.Equals(false)
+    select post).OrderBy(x => x.name);
+
+
+
+    Console.WriteLine("------------------------International----------------------------");
+
+    foreach (var group in groupByOriginInernational)
+    {
+
+        if (group.weight == 0)
+        {
+            group.weight.ToString().Replace("0", "N/A");
+
+        }
+
+        Console.WriteLine($" \n {group.name}\n {group.price}$ \n {group.description} \n Weight {group.weight}g");
+
+        totalInternational += (double)group.price;
+        countInternational++;
+    }
+
+    Console.WriteLine($"The Domestic cost is: {totalDomestic} \n The International cost is: {totalInternational} \n the Domestic count: {countDomestic} \n the International count: {countInternational}");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    throw;
+}
+finally
+{
+    client.Dispose();
+}
---
+++ Receipt/Root.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace Receipt
+{
+    public class Root
+    {
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("domestic")]
+        public bool domestic { get; set; }
+
+        [JsonProperty("price")]
+        public double price { get; set; }
+
+        [JsonProperty("weight")]
+        public int weight { get; set; }
+
+        [JsonProperty("description")]
+        public string description { get; set; }
+
+    }
+}
